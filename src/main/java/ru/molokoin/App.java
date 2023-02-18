ba779957e8e408d77ce2945871797a3713efd73a
esoe <igor_nikulin@mail.ru>
2023-02-18 15:36:24 +0300

    qq

--- src/main/java/ru/molokoin/App.java
+++ src/main/java/ru/molokoin/App.java
@@ -3,7 +3,6 @@ package ru.molokoin;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -13,7 +12,6 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 
 /**
  * Авторизация
@@ -30,13 +28,29 @@ public class App extends Application {
         PasswordField fieldPass = new PasswordField();
         Button buttonSignin = new Button("sign in");
 
+        /**
+         * Обработка клика "sign in"
+         * 1. считываются поля формы (fieldName, fieldPass), пишутся в объект User
+         * 2. Подключение к базе данных
+         * 3. Поиск в базе нужного пользователя
+         */
         buttonSignin.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                Mainframe mainframe = new Mainframe();
-                mainframe.setX(stage.getX() + 300);
-				mainframe.setY(stage.getY() - 200);
-                mainframe.show();
+                //считываем данные пользователя
+                User user = new User(fieldName.getText(), fieldPass.getText());
+                //проверяем наличие прав у пользователя
+                Base base = new Base(user);
+                //открывается, если права пользователя подтверждены
+                if (base.granted){
+                    textWelcome.setText("Доступ предоставлен...");
+                    Mainframe mainframe = new Mainframe();
+                    mainframe.setX(stage.getX() + 300);
+				    mainframe.setY(stage.getY() - 200);
+                    mainframe.show();
+                } else {
+                    textWelcome.setText("В доступе отказано...");
+                }
 			}
         });
 
@@ -47,7 +61,6 @@ public class App extends Application {
         root.add(labelPassword, 0, 2);
         root.add(fieldPass, 1, 2);
         root.add(buttonSignin,0,4,2,1);
-
         Scene scene = new Scene(root, 212, 102);
         stage.setScene(scene);
         stage.show();
@@ -56,5 +69,4 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
